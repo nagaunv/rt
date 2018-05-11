fd10629ab847224b6e74c018f2da7f3f0b596576
nagaunv <naga@cavelab.cs.tsukuba.ac.jp>
2018-05-11 20:11:53 +0900

    [追加] 外部依存

---
+++ external/external.cmake
@@ -0,0 +1,25 @@
+# glfw
+message(STATUS "initializing glfw")
+execute_process(COMMAND git submodule update --init -- external/glfw
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+include_directories(external/glfw/include)
+
+# glm
+message(STATUS "initializing glm")
+execute_process(COMMAND git submodule update --init -- external/glm
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+include_directories(external/glm/glm)
+
+# imgui
+message(STATUS "initializing imgui")
+execute_process(COMMAND git submodule update --init -- external/imgui
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+include_directories(external/imgui)
+
+# vulkan
+message(STATUS "initializing vulkan")
+execute_process(COMMAND git submodule update --init -- external/Vulkan-LoaderAndValidationLayers
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+include_directories(external/Vulkan-LoaderAndValidationLayers/include)
+
+
