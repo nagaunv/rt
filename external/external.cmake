8236f132b9fd8e483457553b5b57b1b7e2b5eab5
nagaunv <naga@cavelab.cs.tsukuba.ac.jp>
2018-05-13 18:16:30 +0900

    [追加] 外部依存

--- external/external.cmake
+++ external/external.cmake
@@ -24,4 +24,16 @@ execute_process(COMMAND git submodule update --init -- external/imgui
                 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 include_directories(external/imgui)
 
+# range-v3
+message(STATUS "initializing range-v3")
+execute_process(COMMAND git submodule update --init -- external/range-v3
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+include_directories(external/range-v3/include)
+
+# selene
+message(STATUS "initializing selene")
+execute_process(COMMAND git submodule update --init -- external/selene
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+include_directories(external/selene)
+
 message(STATUS "------------------------------------------")
