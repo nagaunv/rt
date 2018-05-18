14a21c8ad479d7d41c742bdcee2e8a9ee1d14973
nagaunv <naga@cavelab.cs.tsukuba.ac.jp>
2018-05-18 11:53:56 +0900

    [追加] primitive.hpp

---
+++ src/primitive.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "geometry.hpp"
+#include "material.hpp"
+#include "ray.hpp"
+#include "intersection.hpp"
+#include "bounds.hpp"
+
+namespace naga::rt {
+  /// Primitive
+  class Primitive {
+  public:
+    /// Get material
+    virtual std::shared_ptr<Material> getMaterial() = 0;
+    /// Get area light
+    virtual std::shared_ptr<AreaLight> getAreaLight() = 0;
+    /// Get shape
+    virtual std::shared_ptr<Shape> getShape() = 0;
+    /// Dtor
+    virtual ~Primitive(){};
+  };
+
+  /// GeometrycPrimitive
+  class GeometricPrimitive : public Primitive {
+    /// Get material
+    virtual std::shared_ptr<Material> getMaterial() override;
+    /// Get area light
+    virtual std::shared_ptr<AreaLight> getAreaLight() override;
+    /// Get shape
+    virtual std::shared_ptr<Shape> getShape() override;
+    /// Dtor
+    virtual ~GeometricPrimitive() override;
+
+  private:
+    /// Metarial
+    std::shared_ptr<Material> m_material;
+    /// AreaLight
+    std::shared_ptr<AreaLight> m_areaLight;
+    /// Shape
+    std::shared_ptr<Shape> m_shape
+  };
+}
