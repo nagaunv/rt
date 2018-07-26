c13f424ece86763e5aba55238cbf2001d7e639c1
nagaunv <naga@cavelab.cs.tsukuba.ac.jp>
2018-07-26 18:40:39 +0900

    [追加] lerpを追加

--- src/float.hpp
+++ src/float.hpp
@@ -8,4 +8,36 @@ namespace naga::rt {
 
   /// define float_t
   using float_t = float;
+
+  /// fma
+  template <class FP>
+  constexpr FP fma(FP x, FP y, FP z) {
+    return x * y + z;
+  }
+
+  /// fnma
+  template <class FP>
+  constexpr FP fnma(FP x, FP y, FP z) {
+    return -(x * y + z);
+  }
+  /// fms
+  template <class FP>
+  constexpr FP fms(FP x, FP y, FP z) {
+    return x * y - z;
+  }
+  /// fnms
+  template <class FP>
+  constexpr FP fnms(FP x, FP y, FP z) {
+    return -(x * y - z);
+  }
+
+  /// lerp
+  template <class FP, class V0, class V1>
+  constexpr FP lerp(FP t, V0 v0, V1 v1) {
+    //  vfnmadd213ss    xmm1, xmm0, xmm1
+    //  vfmadd213ss     xmm0, xmm2, xmm1
+    return fma(
+      t, static_cast<FP>(v1),
+      fnms(t, static_cast<FP>(v0), static_cast<FP>(v0)));
+  }
 }
